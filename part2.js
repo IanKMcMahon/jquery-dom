6ac299fd8d2b827c5abdb41ba79d1f547aee79f5
IanKMcMahon <136299057+IanKMcMahon@users.noreply.github.com>
2023-08-27 23:43:25 +0000

    Add JS to delete button

--- part2.js
+++ part2.js
@@ -16,8 +16,8 @@ $(function(){
     
     $("#rating-form").on("submit", function(evt){
         evt.preventDefault();
-        let title = $("#title");
-        let rating = $("#rating");
+        let title = $("#title").val();
+        let rating = $("#rating").val();
 
         let movieData = {title, rating, currentID};
         const HTMLtoAppend = createMovieDataHTML(moviedata);
@@ -29,6 +29,37 @@ $(function(){
     $("#rating-form").trigger("reset");
     })       
     
-})
+});
+
+    $(".btn-danger").on("click", function(evt) {
+        let indexToRemoveAt = movieList.findIndex(movie => movie.currentId === +$(evt.target).data("deleteId"))
+    })
+
+    movieList.splice(indexToRemoveAt, 1)
+
+    $(evt.target)
+      .closest("tr")
+      .remove();
 
 
+
+
+
+
+
+///
+
+function createMovieDataHTML(data) {
+    return `
+      <tr>
+        <td>${data.title}</td>
+        <td>${data.rating}</td>
+        <td>
+          <button class="btn btn-danger" data-delete-id=${data.currentId}>
+            Delete
+          </button>
+        </td>
+      <tr>
+    `;
+  }
+  
